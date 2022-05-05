21762d94d4a00ee1c21d4da592827cadb35cb772
nice77 <n.i.y.a.z.19831977@gmail.com>
2022-05-05 15:37:12 +0300

    added serve function

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 from app import app
-# from waitress import serve
+from waitress import serve
 
 
 if __name__ == "__main__":
-	app.run(port=8080)
-	# serve(app, host='0.0.0.0', port=5000)
+	# app.run(port=8080)
+	serve(app, host='0.0.0.0', port=8080)
